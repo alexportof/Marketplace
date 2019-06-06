674a2d8567d3789d31410c31b0983189cf7e2dff
Álex Porto <alexportoferreira@hotmail.com>
2019-06-06 12:27:33 -0300

    Add password encryption with bcryptjs

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -21,4 +22,10 @@ const UserSchema = new mongoose.Schema({
   }
 })
 
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next()
+  }
+  this.password = await bcrypt.hash(this.password, 8)
+})
 module.exports = mongoose.model('User', UserSchema)
